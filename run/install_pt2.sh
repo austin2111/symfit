bf062059e48d248fe45236ac82e18eaa199e24a8
austin2111 <159757388+austin2111@users.noreply.github.com>
2026-02-04 13:12:06 -0800

    Update install_pt2.sh
    Added support for building symcc_fuzzing_helper

--- run/install_pt2.sh
+++ run/install_pt2.sh
@@ -16,4 +16,11 @@ mkdir symcc_build symsan_build symfit_symsan_build
 ./compile.sh --symcc
 ./compile.sh --symsan
 ./compile.sh --symfit_symsan
+
+echo "Installing patched symcc_fuzzing_helper utility..."
+
+patch /workdir/symcc/util/symcc_fuzzing_helper/src/main.rs /workdir/symfit/symcc_helper.patch
+cargo build --manifest-path /workdir/symcc/util/symcc_fuzzing_helper/Cargo.toml --target-dir /workdir/symcc/util/symcc_fuzzing_helper
+cp /workdir/symcc/util/symcc_fuzzing_helper/debug/symcc_fuzzing_helper /workdir/
+
 echo "Installation complete! In the future, please run launch.sh before entering the Symfit environment. To get started, try running an emulator boot script."
